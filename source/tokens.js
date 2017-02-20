4afc642a49aa6b4468f40e9fc5d6ad6b21aed579
Joe Grund <joe.grund@intel.com>
2017-02-20 08:03:46 -0500

    Bump to latest fp
    qs-parsers needs the newest fp to work with newest flowtype.
    Bump to latest fp, in addition add prettier and reformat
    the code. Finally, stop transpiling to ES5.
    
      - Bump deps.
      - Add prettier.
      - Stop transpiling.
      - Switch to Jest.
    
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- source/tokens.js
+++ source/tokens.js
@@ -3,7 +3,7 @@
 //
 // INTEL CONFIDENTIAL
 //
-// Copyright 2013-2016 Intel Corporation All Rights Reserved.
+// Copyright 2013-2017 Intel Corporation All Rights Reserved.
 //
 // The source code contained or described herein and all documents related
 // to the source code ("Material") are owned by Intel Corporation or its
@@ -21,7 +21,7 @@
 // otherwise. Any license under such intellectual property rights must be
 // express and approved by Intel in writing.
 
-import * as parsely from 'intel-parsely';
+import * as parsely from '@iml/parsely';
 
 export const qsToInputTokens = [
   {
