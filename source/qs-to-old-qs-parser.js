0a890434fa687e002e1211dcd1ef54bb89d22240
Will Johnson <wjohnson@whamcloud.com>
2018-06-29 16:47:09 -0400

    Add copyrights back.
    Signed-off-by: Will Johnson <wjohnson@whamcloud.com>

--- source/qs-to-old-qs-parser.js
+++ source/qs-to-old-qs-parser.js
@@ -1,5 +1,10 @@
 // @flow
 
+//
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 import * as fp from '@iml/fp';
 import * as parsely from '@iml/parsely';
 import { dot, dash, YYYY, MM, DD, hh, mm, ss, colon } from './input-to-qs-parser.js';
--- source/tokens.js
+++ source/tokens.js
@@ -1,5 +1,10 @@
 // @flow
 
+//
+// Copyright (c) 2017 Intel Corporation. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
 import * as parsely from '@iml/parsely';
 
 export const qsToInputTokens = [
